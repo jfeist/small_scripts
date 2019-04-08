5b9c4959518aff5267999fa84ba01fbb2b4c0d78
Johannes Feist <johannes.feist@gmail.com>
2019-04-08 15:00:33 +0200

    run commands in setup_wireguard_server.sh independently to prevent problems if one fails (such as pfctl -d when no firewall is running)

--- setup_wireguard_server.sh
+++ setup_wireguard_server.sh
@@ -1,1 +1,6 @@
-wg-quick up jf && sudo sysctl -w net.inet.ip.forwarding=1 && sudo pfctl -d && sudo pfctl -e -f /etc/pf.conf
+set -x
+
+wg-quick up jf
+sudo sysctl -w net.inet.ip.forwarding=1
+sudo pfctl -d
+sudo pfctl -e -f /etc/pf.conf
