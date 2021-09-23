7b5a62c85be9e8c7a2d1fc9d4f233a4c71af1092
Johannes Feist <johannes.feist@gmail.com>
2021-09-23 09:37:03 +0200

    change new_paper_git.sh to use gh instead of hub

--- new_paper_git.sh
+++ new_paper_git.sh
@@ -1,4 +1,4 @@
-#!/opt/local/bin/bash
+#!/usr/bin/env bash
 
 if [[ $# == 0 ]]; then
     echo usage: "$0 paper_name"
@@ -6,35 +6,16 @@ if [[ $# == 0 ]]; then
 fi
 
 name="$1"; shift
-mkdir "$name"
+
+gh repo create -y --gitignore TeX --private "tex.papers.${name}"
+mv "tex.papers.${name}" "$name"
 cd "$name"
-git init
-
-cat > .gitignore <<EOF
-*~
-diff.html
-
-*.aux
-*.bbl
-*.blg
-*.fdb_latexmk
-*.fls
-*.log
-*.out
-*.rel
-*.synctex.gz
-*.tps
-*.upb
-*Notes.bib
-EOF
 
 cat > .gitattributes <<EOF
 *.tex diff=tex
 EOF
 
-git add .gitignore
 git add .gitattributes
-git ci -m 'initial commit with only .gitignore/.gitattributes'
-hub create -p "tex.papers.${name}"
-git remote rename origin jf
-git push -u jf main
+
+git ci --amend -m 'initial commit with only .gitignore/.gitattributes'
+git push -f
