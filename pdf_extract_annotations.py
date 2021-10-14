5d13e83e1630cfd6aec3efc237e88a4d5000a239
Johannes Feist <johannes.feist@gmail.com>
2021-10-14 16:26:13 +0200

    update pdf_extract_annotations to work with python from path (for anaconda on macOS: "sudo port install poppler-qt5; pip install python-poppler-qt5")

--- pdf_extract_annotations.py
+++ pdf_extract_annotations.py
@@ -1,30 +1,37 @@
-#!/opt/local/bin/python3.9
+#!/usr/bin/env python
 
-from popplerqt5 import Poppler
-import sys
+import os, sys
 
-def indent(text, amount, ch=' '):
-    padding = amount * ch
-    return ''.join((i!=0)*padding+line for i,line in enumerate(text.splitlines(True)))
+# workaround for a problem with Qt5 wrongly checking macOS 11 version
+if not os.getenv("SYSTEM_VERSION_COMPAT") == "0":
+    os.environ["SYSTEM_VERSION_COMPAT"] = "0"
+    import subprocess
+    subprocess.run(sys.argv)
+else:
+    from popplerqt5 import Poppler
 
-def main():
-    input_filename = sys.argv[1]
-    document = Poppler.Document.load(input_filename)
-    n_annots = 0
+    def indent(text, amount, ch=' '):
+        padding = amount * ch
+        return ''.join((i!=0)*padding+line for i,line in enumerate(text.splitlines(True)))
 
-    for i in range(len(document)):
-        page = document.page(i)
-        for annot in page.annotations():
-            if not isinstance(annot,Poppler.LinkAnnotation):
-                n_annots += 1
-                contents = annot.contents()
-                annottype = type(annot).__name__.replace("Annotation","")
-                print(indent('page %3s, %10s: %s' % (i+1, annottype, contents), 22))
+    def main():
+        input_filename = sys.argv[1]
+        document = Poppler.Document.load(input_filename)
+        n_annots = 0
 
-    # if n_annots > 0:
-    #   print n_annots, "annotation(s) found"
-    # else:
-    #   print "no annotations found"
+        for i in range(len(document)):
+            page = document.page(i)
+            for annot in page.annotations():
+                if not isinstance(annot,Poppler.LinkAnnotation):
+                    n_annots += 1
+                    contents = annot.contents()
+                    annottype = type(annot).__name__.replace("Annotation","")
+                    print(indent('page %3s, %10s: %s' % (i+1, annottype, contents), 22))
 
-if __name__ == "__main__":
-    main()
+        # if n_annots > 0:
+        #   print n_annots, "annotation(s) found"
+        # else:
+        #   print "no annotations found"
+
+    if __name__ == "__main__":
+        main()
