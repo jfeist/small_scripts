035e1bceecd70a43bd71637351ed847b62b903be
Johannes Feist <johannes.feist@gmail.com>
2021-10-15 20:35:30 +0200

    in pdf_extract_annotations.py, also show the highlighted text for highlight annotations

--- pdf_extract_annotations.py
+++ pdf_extract_annotations.py
@@ -6,32 +6,57 @@ import os, sys
 if not os.getenv("SYSTEM_VERSION_COMPAT") == "0":
     os.environ["SYSTEM_VERSION_COMPAT"] = "0"
     import subprocess
-    subprocess.run(sys.argv)
-else:
-    from popplerqt5 import Poppler
+    proc = subprocess.run(sys.argv)
+    sys.exit(proc.returncode)
 
-    def indent(text, amount, ch=' '):
-        padding = amount * ch
-        return ''.join((i!=0)*padding+line for i,line in enumerate(text.splitlines(True)))
+# start of actual script
+from popplerqt5 import Poppler
+from PyQt5.QtCore import QRectF, QPointF
+from textwrap import fill
 
-    def main():
-        input_filename = sys.argv[1]
-        document = Poppler.Document.load(input_filename)
-        n_annots = 0
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[34m'
+    OKCYAN = '\033[36m'
+    OKGREEN = '\033[32m'
+    WARNING = '\033[33m'
+    FAIL = '\033[31m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
-        for i in range(len(document)):
-            page = document.page(i)
-            for annot in page.annotations():
-                if not isinstance(annot,Poppler.LinkAnnotation):
-                    n_annots += 1
-                    contents = annot.contents()
-                    annottype = type(annot).__name__.replace("Annotation","")
-                    print(indent('page %3s, %10s: %s' % (i+1, annottype, contents), 22))
+def indent(text, amount, ch=' '):
+    padding = amount * ch
+    return ''.join((i!=0)*padding+line for i,line in enumerate(text.splitlines(True)))
 
-        # if n_annots > 0:
-        #   print n_annots, "annotation(s) found"
-        # else:
-        #   print "no annotations found"
+printind22 = lambda text: print(indent(text,22))
 
-    if __name__ == "__main__":
-        main()
+def main():
+    input_filename = sys.argv[1]
+    document = Poppler.Document.load(input_filename)
+    n_annots = 0
+
+    for i in range(len(document)):
+        page = document.page(i)
+        scaleP = lambda P: QPointF(P.x() * page.pageSize().width(), P.y() * page.pageSize().height())
+        scaleQ = lambda Q: QRectF(scaleP(Q.points[0]),scaleP(Q.points[2]))
+
+        for annot in page.annotations():
+            if not isinstance(annot,Poppler.LinkAnnotation):
+                n_annots += 1
+                contents = annot.contents()
+                annottype = type(annot).__name__.replace("Annotation","")
+                printind22(f'{bcolors.OKBLUE}page {i+1:3d}, {annottype:>10s}{bcolors.ENDC}: {fill(contents,100)}')
+                if isinstance(annot,Poppler.HighlightAnnotation):
+                    quads = annot.highlightQuads()
+                    txt = " ".join([page.text(scaleQ(quad)).strip() for quad in quads])
+                    printind22(f'    {bcolors.OKGREEN}highlighted text:{bcolors.ENDC} {fill(txt,100)}')
+                print()
+
+    # if n_annots > 0:
+    #   print n_annots, "annotation(s) found"
+    # else:
+    #   print "no annotations found"
+
+if __name__ == "__main__":
+    main()
