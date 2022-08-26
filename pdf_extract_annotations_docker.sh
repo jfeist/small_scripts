6246f5097266cf8e66cacfed0d8078317235fd5c
Johannes Feist <johannes.feist@gmail.com>
2022-08-26 12:25:37 +0200

    add Dockerfile for pdf_extract_annotations and script to use it (instead of needing to do rsync to escape.ftmc.uam.es)

---
+++ pdf_extract_annotations_docker.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 PDFFILE" >&2
+  exit 1
+fi
+
+PDFFILE=$1; shift
+
+docker run -it -e PDF_PAGESHIFT="${PDF_PAGESHIFT:-0}" -v "${PWD}:/tmp" jfeist/pdf_extract_annotations "${PDFFILE}" | grep -v '^"Error: ' | unicodeit.py | ansi2html.sh > annot.html
