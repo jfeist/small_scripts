6a4382535fc3791aadae6f9b974a8ed6e2ac97da
Johannes Feist <johannes.feist@gmail.com>
2022-09-06 10:31:40 +0200

    add --rm for docker run in pdf_extract_annotations_docker.sh

--- pdf_extract_annotations_docker.sh
+++ pdf_extract_annotations_docker.sh
@@ -7,4 +7,4 @@ fi
 
 PDFFILE=$1; shift
 
-docker run -it -e PDF_PAGESHIFT="${PDF_PAGESHIFT:-0}" -v "${PWD}:/tmp" jfeist/pdf_extract_annotations "${PDFFILE}" | grep -v '^"Error: ' | unicodeit.py | ansi2html.sh > annot.html
+docker run -it --rm -e PDF_PAGESHIFT="${PDF_PAGESHIFT:-0}" -v "${PWD}:/tmp" jfeist/pdf_extract_annotations "${PDFFILE}" | grep -v '^"Error: ' | unicodeit.py | ansi2html.sh > annot.html
