c1d6f1fabf19ab8e3999ed83eedf979d4ba982de
Johannes Feist <johannes.feist@gmail.com>
2025-07-15 10:45:05 +0200

    add list_latex_dependencies.sh to get all files used by .tex files

---
+++ list_latex_dependencies.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -e
+# set -x
+
+# from https://unix.stackexchange.com/questions/230047/how-to-list-all-targets-in-make
+
+depfile=LATEX_DEPENDENCIES
+
+latexmk -deps-out=${depfile} "$@" > /dev/null
+
+targets=$(make -qp -f ${depfile} | awk -F':' '/^[a-zA-Z0-9][^$#\/\t=]*:([^=]|$)/ {split($1,A,/ /);for(i in A)print A[i]}' | sort -u | grep -v -e "${depfile}" -e '.*Notes.bib')
+
+texpdfs=$(grep '\.tex$' <<< "$targets" | sed 's/\.tex$/.pdf/')
+
+# print the list of targets without the pdf files generated from .tex files
+echo "$targets" | grep -v -e "${texpdfs}"
+
+rm ${depfile}
